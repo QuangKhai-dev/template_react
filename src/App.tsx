a14caad02b58eaed6e60276a52aa6a5f214058af
Đỗ Quang Khải <88335848+QuangKhai-dev@users.noreply.github.com>
2024-12-03 04:09:43 +0700

    setup template

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <div>
+          <h1>Hello World</h1>
+        </div>
+      ),
+    },
+    {
+      path: "about",
+      element: <div>About</div>,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
